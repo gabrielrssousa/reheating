44318e8dd8c5c27d0882efc384bf80cf9eb3b2e8
Gabriel Sousa <gabriel.mywork@proton.me>
2025-05-19 14:47:55 -0300

    adding spring initializr project

---
+++ src/main/java/com/gabrielrssousa/reheating/ReheatingApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielrssousa.reheating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReheatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReheatingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielrssousa/reheating/ReheatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielrssousa.reheating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReheatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
